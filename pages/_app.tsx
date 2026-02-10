38265311b2d16b581c282f210f75da75fa29219d
Yogesh Vadivel <yogesh.vg456@gmail.com>
2026-02-10 21:05:23 +0400

    Route all pages through SPA in _app and remove conflicting app subroutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
+ "use client"
+
 import type { AppProps } from "next/app"
 import "@/app/globals.css"
 import { WebSocketProvider } from "@/hooks/use-websocket-context"
+import App from "@/app_v0modified"
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp(_: AppProps) {
   return (
     <WebSocketProvider>
-      <Component {...pageProps} />
+      <App />
     </WebSocketProvider>
   )
 }
+
 
